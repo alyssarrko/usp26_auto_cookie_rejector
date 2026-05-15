d0da79ce867ef91dd5afb8933ba4b350ebb11419
IamJWGit <jw4540@columbia.edu>
2026-05-15 00:26:48 -0400

    Update sw.js to check current badge text to stop Nike flickering loop

--- sw.js
+++ sw.js
@@ -110,7 +110,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
   // 1. Handle Status Updates (Check text first to stop Nike flickering)
   if (message.status) {
-    updateBadge(tabId, message.status);
+    chrome.action.getBadgeText({ tabId }).then(currentText => {
+      const nextText = message.status === "success" ? "OK" : "!";
+      if (currentText !== nextText) {
+        updateBadge(tabId, message.status);
+      }
+    }).catch(() => updateBadge(tabId, message.status));
     
     if (message.type === "update-status") {
       sendResponse({ ok: true });
